c8ff6ca825c12ae779001e0c10f76a3fdddd8fb9
goreleaserbot <goreleaser@carlosbecker.com>
2021-09-29 16:35:21 +0200

    Brew formula update for fritz-tls version v0.10.0

--- Formula/fritz-tls.rb
+++ Formula/fritz-tls.rb
@@ -5,28 +5,28 @@
 class FritzTls < Formula
   desc "Automate TLS certificate installation for AVM FRITZ!Box"
   homepage "https://github.com/tisba/fritz-tls"
-  version "0.9.0"
+  version "0.10.0"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/tisba/fritz-tls/releases/download/v0.9.0/fritz-tls_0.9.0_darwin_x86_64.zip"
-      sha256 "1fbcf4002ef8576f9a644016ebbfaacc9e48a817f8025220ede69612ad63b2cc"
+      url "https://github.com/tisba/fritz-tls/releases/download/v0.10.0/fritz-tls_0.10.0_darwin_x86_64.zip"
+      sha256 "0887e12e4864b8ea2c4d4e4a4c61d01d2550db5d9f8464bdcfdb335c900689aa"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/tisba/fritz-tls/releases/download/v0.9.0/fritz-tls_0.9.0_darwin_arm64.zip"
-      sha256 "84fe979fb36510dcc165b6cbe8b4b4a39ff72569cd4019a9a8bc0e87045789ad"
+      url "https://github.com/tisba/fritz-tls/releases/download/v0.10.0/fritz-tls_0.10.0_darwin_arm64.zip"
+      sha256 "07f2b395e2c814488a6d0ab386728438671c3f0ddb7e18cf1d72bef46052a873"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/tisba/fritz-tls/releases/download/v0.9.0/fritz-tls_0.9.0_linux_x86_64.tar.gz"
-      sha256 "5d241f3222c1e7cb7879b38ac488fb912a2ffaa400c19276ba84d842dce6a23e"
+      url "https://github.com/tisba/fritz-tls/releases/download/v0.10.0/fritz-tls_0.10.0_linux_x86_64.tar.gz"
+      sha256 "87a35dda96387db483df2896f1da2de7273c5d5649cfa69cd3669c8df140861d"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/tisba/fritz-tls/releases/download/v0.9.0/fritz-tls_0.9.0_linux_arm64.tar.gz"
-      sha256 "4cb88d318d2bdcf92cb9f60a0a03072591f7aacc2cb247a7baaa9d42fc1a5189"
+      url "https://github.com/tisba/fritz-tls/releases/download/v0.10.0/fritz-tls_0.10.0_linux_arm64.tar.gz"
+      sha256 "f68581606de51c136bca10c6e510a7915a7df862f0b69fe8244784ce5e7b8121"
     end
   end
 
